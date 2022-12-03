c56d3531b86fc61c4b83ac0e6237f7e3c7f84136
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-03 09:06:35 +0100

    Day 3 - part two

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -3,7 +3,6 @@
 
 use std::io;
 
-
 fn read_string() -> Option<String> {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
@@ -28,17 +27,27 @@ fn char_priority(ch1: u8) -> i32 {
 }
 
 const NUM_OF_CHARS: usize = 256;
+const NON_OF_ELVES: i32 = -1;
 
 fn main() {
     let mut priorities = 0;
+    let mut badge_priorities = 0;
+    let mut elf_number = 0;
+    let mut badge_tab = vec![NON_OF_ELVES; NUM_OF_CHARS];
 
     loop {
         match read_string() {
             None => break,
             Some(s) => {
                 let mut char_tab = vec![false; NUM_OF_CHARS];
-                let half_size = s.as_bytes().len() / 2;
 
+                for ch in s.as_bytes() {
+                    if badge_tab[*ch as usize] == elf_number - 1 {
+                        badge_tab[*ch as usize] = elf_number;
+                    }
+                }
+
+                let half_size = s.as_bytes().len() / 2;
                 for ch in &s.as_bytes()[..half_size] {
                     char_tab[*ch as usize] = true;
                 }
@@ -49,9 +58,22 @@ fn main() {
                         break;
                     }
                 }
+
+                if elf_number == 2 {
+                    for idx in 0..NUM_OF_CHARS {
+                        if badge_tab[idx] == 2 {
+                            badge_priorities += char_priority(idx as u8);
+                        }
+                    }
+
+                    badge_tab = vec![NON_OF_ELVES; NUM_OF_CHARS];
+                }
+
+                elf_number = (elf_number + 1) % 3;
             }
         }
     }
 
     println!("{}", priorities);
+    println!("{}", badge_priorities);
 }
