bfe026c723bdf8fbf6fa3a824399e1ca60d8b1ad
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-06 09:01:28 +0100

    Day 6 - part 2

--- 2022/day06/src/main.rs
+++ 2022/day06/src/main.rs
@@ -12,24 +12,36 @@ fn read_string() -> String {
 }
 
 fn main() {
-    let mut result = 0;
-    let mut window = VecDeque::new();
+    let mut result1 = None;
+    let mut result2 = None;
+    let mut window1 = VecDeque::new();
+    let mut window2 = VecDeque::new();
 
     let input = read_string();
 
     for (idx, ch) in input.chars().enumerate() {
-        window.push_back(ch);
+        window1.push_back(ch);
+        window2.push_back(ch);
 
-        if window.len() > 4 {
-            window.pop_front();
+        if result1.is_none() && window1.len() > 4 {
+            window1.pop_front();
 
-            let letters: HashSet<char> = window.iter().cloned().collect();
+            let letters: HashSet<char> = window1.iter().cloned().collect();
             if letters.len() == 4 {
-                result = idx + 1;
-                break;
+                result1 = Some(idx + 1);
+            }
+        }
+
+        if result2.is_none() && window2.len() > 14 {
+            window2.pop_front();
+
+            let letters: HashSet<char> = window2.iter().cloned().collect();
+            if result2.is_none() && letters.len() == 14 {
+                result2 = Some(idx + 1);
             }
         }
     }
 
-    println!("{}", result);
+    println!("{}", result1.unwrap());
+    println!("{}", result2.unwrap());
 }
