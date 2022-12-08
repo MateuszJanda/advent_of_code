8d73cacfddb872b9a49bccbb96e49e7f42872e3b
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-08 10:22:12 +0100

    Wip

--- 2022/day08/src/main.rs
+++ 2022/day08/src/main.rs
@@ -23,26 +23,65 @@ fn read_nums() -> Option<Vec<i32>> {
 }
 
 fn main() {
-    println!("Hello world!");
+    let mut trees = vec![];
+    let mut mark = vec![];
+    let mut visible = 0;
 
     let mut height_top = vec![];
     let mut height_left = vec![];
+
     let mut row = 0;
     while let Some(nums) = read_nums() {
+        mark.push(vec![false; nums.len()]);
+
         for (col, val) in nums.iter().enumerate() {
             if row == 0 {
-                height_top.push(val);
-            } else {
-                height_top[col] = std::cmp::max(height_top[col], val);
+                height_top.push(-1);
+                visible += 1;
+                mark[row][col] = true;
+            } else if *val > height_top[col] {
+                visible += 1;
+                mark[row][col] = true;
             }
 
             if col == 0 {
-                height_left.push(val);
-            } else {
-                height_left[row] = std::cmp::max(height_left[col], val);
+                height_left.push(-1);
+                if mark[row][col] == false {
+                    visible += 1;
+                    mark[row][col] = true;
+                }
+            } else if mark[row][col] == false && *val > height_top[row] {
+                visible += 1;
+                mark[row][col] = true;
             }
+
+            height_top[col] = std::cmp::max(height_top[col], *val);
+            height_left[row] = std::cmp::max(height_left[row], *val);
         }
 
+        trees.push(nums);
         row += 1;
     }
+
+    let mut height_right = vec![-1; trees.len()];
+    let mut height_bottom = vec![-1; trees[0].len()];
+
+    for row in (0..trees.len()).rev() {
+        for col in (0..trees[0].len()).rev() {
+            let val = trees[row][col];
+
+            if mark[row][col] == false && val > height_right[row] {
+                visible += 1;
+                mark[row][col] = true;
+            } else if mark[row][col] == false && val > height_bottom[col] {
+                visible += 1;
+                mark[row][col] = true;
+            }
+
+            height_bottom[col] = std::cmp::max(height_bottom[col], val);
+            height_right[row] = std::cmp::max(height_right[row], val);
+        }
+    }
+
+    println!("{}", visible);
 }
