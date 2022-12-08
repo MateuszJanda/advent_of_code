790a493064c6cd11c6b07bbcf5107422b2e11523
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-08 10:39:34 +0100

    Day 8 - part 1

--- 2022/day08/src/main.rs
+++ 2022/day08/src/main.rs
@@ -22,44 +22,61 @@ fn read_nums() -> Option<Vec<i32>> {
     }
 }
 
+struct Tree {
+    value: i32,
+    height_top: i32,
+    height_bottom: i32,
+    height_right: i32,
+    height_left: i32,
+}
+
+impl Tree {
+    fn new(value: i32) -> Self {
+        Tree {
+            value,
+            height_top: -1,
+            height_bottom: -1,
+            height_right: -1,
+            height_left: -1,
+        }
+    }
+
+    fn is_visible(&self) -> bool {
+        self.value > self.height_top
+            || self.value > self.height_bottom
+            || self.value > self.height_right
+            || self.value > self.height_left
+    }
+}
+
 fn main() {
     let mut trees = vec![];
-    let mut mark = vec![];
-    let mut visible = 0;
 
     let mut height_top = vec![];
     let mut height_left = vec![];
 
     let mut row = 0;
     while let Some(nums) = read_nums() {
-        mark.push(vec![false; nums.len()]);
-
+        let mut tree_row = vec![];
         for (col, val) in nums.iter().enumerate() {
             if row == 0 {
                 height_top.push(-1);
-                visible += 1;
-                mark[row][col] = true;
-            } else if *val > height_top[col] {
-                visible += 1;
-                mark[row][col] = true;
             }
 
             if col == 0 {
                 height_left.push(-1);
-                if mark[row][col] == false {
-                    visible += 1;
-                    mark[row][col] = true;
-                }
-            } else if mark[row][col] == false && *val > height_top[row] {
-                visible += 1;
-                mark[row][col] = true;
             }
+
+            let mut tree = Tree::new(*val);
+            tree.height_top = height_top[col];
+            tree.height_left = height_left[row];
+            tree_row.push(tree);
 
             height_top[col] = std::cmp::max(height_top[col], *val);
             height_left[row] = std::cmp::max(height_left[row], *val);
         }
 
-        trees.push(nums);
+        trees.push(tree_row);
         row += 1;
     }
 
@@ -68,20 +85,26 @@ fn main() {
 
     for row in (0..trees.len()).rev() {
         for col in (0..trees[0].len()).rev() {
-            let val = trees[row][col];
+            let mut tree = &mut trees[row][col];
 
-            if mark[row][col] == false && val > height_right[row] {
+            tree.height_bottom = height_bottom[col];
+            tree.height_right = height_right[row];
+
+            height_bottom[col] = std::cmp::max(height_bottom[col], tree.value);
+            height_right[row] = std::cmp::max(height_right[row], tree.value);
+        }
+    }
+
+    let mut visible = 0;
+    for row in (0..trees.len()).rev() {
+        for col in (0..trees[0].len()).rev() {
+            if trees[row][col].is_visible() {
                 visible += 1;
-                mark[row][col] = true;
-            } else if mark[row][col] == false && val > height_bottom[col] {
-                visible += 1;
-                mark[row][col] = true;
             }
-
-            height_bottom[col] = std::cmp::max(height_bottom[col], val);
-            height_right[row] = std::cmp::max(height_right[row], val);
         }
     }
 
     println!("{}", visible);
 }
+
+
