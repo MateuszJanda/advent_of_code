2de952d8d113bb24e6bfed2273e6680c29525aeb
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-09 11:24:23 +0100

    Day 9 - part 1

--- 2022/day09/src/main.rs
+++ 2022/day09/src/main.rs
@@ -24,7 +24,7 @@ fn read_motions() -> Option<(char, i32)> {
     }
 }
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Eq, Hash, PartialEq, Clone)]
 struct Position {
     x: i32,
     y: i32,
@@ -34,15 +34,106 @@ impl Position {
     fn new(x: i32, y: i32) -> Self {
         Position { x, y }
     }
+
+    fn is_close(&self, other: &Position) -> bool {
+        let x_delta = (self.x - other.x).abs();
+        let y_delta = (self.y - other.y).abs();
+
+        (x_delta == 0 || x_delta == 1) && (y_delta == 0 || y_delta == 1)
+    }
+}
+
+fn move_right(
+    head: &mut Position,
+    tail: &mut Position,
+    steps: i32,
+    positions: &mut HashSet<Position>,
+) {
+    let mut old_head = Position::new(head.x, head.y);
+    for _ in 0..steps {
+        head.x += 1;
+
+        if !head.is_close(&tail) {
+            *tail = old_head;
+            positions.insert(tail.clone());
+        }
+
+        old_head = head.clone();
+    }
+}
+
+fn move_left(
+    head: &mut Position,
+    tail: &mut Position,
+    steps: i32,
+    positions: &mut HashSet<Position>,
+) {
+    let mut old_head = Position::new(head.x, head.y);
+    for _ in 0..steps {
+        head.x -= 1;
+
+        if !head.is_close(&tail) {
+            *tail = old_head;
+            positions.insert(tail.clone());
+        }
+
+        old_head = head.clone();
+    }
+}
+
+fn move_up(
+    head: &mut Position,
+    tail: &mut Position,
+    steps: i32,
+    positions: &mut HashSet<Position>,
+) {
+    let mut old_head = Position::new(head.x, head.y);
+    for _ in 0..steps {
+        head.y += 1;
+
+        if !head.is_close(&tail) {
+            *tail = old_head;
+            positions.insert(tail.clone());
+        }
+
+        old_head = head.clone();
+    }
+}
+
+fn move_down(
+    head: &mut Position,
+    tail: &mut Position,
+    steps: i32,
+    positions: &mut HashSet<Position>,
+) {
+    let mut old_head = Position::new(head.x, head.y);
+    for _ in 0..steps {
+        head.y -= 1;
+
+        if !head.is_close(&tail) {
+            *tail = old_head;
+            positions.insert(tail.clone());
+        }
+
+        old_head = head.clone();
+    }
 }
 
 fn main() {
     let mut positions = HashSet::<Position>::new();
     let mut head = Position::new(0, 0);
+    let mut tail = Position::new(0, 0);
+
+    positions.insert(tail.clone());
 
     while let Some((dir, steps)) = read_motions() {
-        let p = Position::new(0, 0);
-        positions.insert(p);
+        match dir {
+            'R' => move_right(&mut head, &mut tail, steps, &mut positions),
+            'L' => move_left(&mut head, &mut tail, steps, &mut positions),
+            'U' => move_up(&mut head, &mut tail, steps, &mut positions),
+            'D' => move_down(&mut head, &mut tail, steps, &mut positions),
+            _ => panic!("Unknown direction"),
+        }
     }
 
     println!("{}", positions.len());
