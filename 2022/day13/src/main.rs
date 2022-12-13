b0339c29ed3639b12f85ff02fdb79355b2898d43
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-13 20:12:24 +0100

    Day 13 - wip

--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -1,9 +1,114 @@
 // Author:  mateusz.janda@gmail.com
 // Ad maiorem Dei gloriam
 
+use std::cmp::Ordering;
+
 use std::io;
 
+fn read_string() -> Option<String> {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    match line.strip_suffix("\n") {
+        None => None,
+        Some(stripped_line) => {
+            if stripped_line.is_empty() {
+                return None;
+            }
+
+            Some(stripped_line.to_string())
+        }
+    }
+}
+
+fn read_packets() -> Option<(String, String)> {
+    let left = read_string()?;
+    let right = read_string()?;
+
+    // Read blank line
+    read_string();
+
+    Some((left, right))
+}
+
+fn split_packet(packet: &str) -> Vec<String> {
+    let mut bracket_counter = 0;
+    let mut start_value = None;
+    let mut result = vec![];
+
+    for (idx, ch) in packet.chars().enumerate() {
+        match ch {
+            '[' => {
+                bracket_counter += 1;
+                if bracket_counter > 1 && start_value.is_none() {
+                    start_value = Some(idx);
+                }
+            }
+            ']' => {
+                if bracket_counter == 1 {
+                    match start_value {
+                        Some(start) => result.push(packet[start..idx].to_string()),
+                        None => result.push("".to_string()),
+                    };
+                    start_value = None;
+                }
+                bracket_counter -= 1;
+            }
+            ',' => {
+                if bracket_counter == 1 {
+                    result.push(packet[start_value.unwrap()..idx].to_string());
+                    start_value = None;
+                }
+            }
+            '0'..='9' => {
+                if bracket_counter == 1 && start_value.is_none() {
+                    start_value = Some(idx);
+                }
+            }
+            _ => panic!("Unknown character"),
+        }
+    }
+
+    result
+}
+
+fn check_order(left: &str, right: &str) -> Ordering {
+    // println!("{:?}", split_packet(left));
+    // println!("{:?}", split_packet(right));
+    // println!("-----");
+
+    let left_vec = split_packet(left);
+    let right_vec = split_packet(right);
+
+    let it = left_vec.iter().zip(right_vec.iter());
+
+    for (l_str, r_str) in it {
+        let result = match (l_str.parse::<i32>(), r_str.parse::<i32>()) {
+            (Ok(l_val), Ok(r_val)) => l_val.cmp(&r_val),
+            (Ok(_), Err(_)) => {
+                check_order(l_str.as_str(), ("[".to_string() + r_str + "]").as_str())
+            }
+            (Err(_), Ok(_)) => {
+                check_order(("[".to_string() + l_str + "]").as_str(), r_str.as_str())
+            }
+            (Err(_), Err(_)) => check_order(
+                ("[".to_string() + l_str + "]").as_str(),
+                ("[".to_string() + r_str + "]").as_str(),
+            ),
+        };
+
+        match result {
+            Ordering::Equal => (),
+            cmp => return cmp,
+        };
+    }
+
+    Ordering::Equal
+}
 
 fn main() {
-    println!("Hello world!")
+    let mut counter = 1;
+    while let Some((left, right)) = read_packets() {
+        check_order(left.as_str(), right.as_str());
+        counter += 1;
+    }
 }
