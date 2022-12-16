c13cb4492114ba070d58cfa74dcb092915c2dc03
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-16 19:32:39 +0100

    Added BFS

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -2,6 +2,8 @@
 // Ad maiorem Dei gloriam
 
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::io;
 use std::iter::FromIterator;
 
@@ -36,30 +38,65 @@ fn read_valve() -> Option<(String, i32, Vec<String>)> {
     }
 }
 
-fn main() {
-    let mut neighborhood = HashMap::new();
+fn bfs(
+    start_valve: &String,
+    mut time: i32,
+    graph: &HashMap<String, Vec<String>>,
+    rates: &HashMap<String, i32>,
+    visited: &HashSet<String>,
+) -> (String, i32) {
+    let mut queue = VecDeque::new();
 
-    while let Some((valve, rate, adjacent)) = read_valve() {
-        println!("{} {} {:?}", valve, rate, adjacent);
+    time -= 1;
+    queue.push_back((start_valve.clone(), time));
 
-        neighborhood.insert(valve, adjacent);
-    }
+    let mut best_time = 0;
+    let mut best_rate = 0;
+    let mut best_valve = "".to_string();
 
-    for (v1, adj1) in neighborhood.iter() {
-        for a1 in adj1.iter() {
-            let mut bi_dir = false;
-            for v2 in neighborhood[a1].iter() {
-                if v1 == v2 {
-                    bi_dir = true;
-                    break;
-                }
-            }
+    while let Some((valve, t)) = queue.pop_front() {
+        if visited.contains(&valve) || t - 1 == 0 {
+            continue;
+        }
 
-            if !bi_dir {
-                println!("No bidirectional {} {}", v1, a1);
-            }
+        let rate = rates[&valve] * (t - 1);
+        if rate > best_rate {
+            best_rate = rate;
+            best_time = t;
+            best_valve = valve.clone();
+        }
+
+        for v in graph[&valve].iter() {
+            queue.push_back((v.clone(), t - 1));
         }
     }
 
-    println!("Ok")
+    (best_valve, best_time)
+}
+
+fn main() {
+    let mut graph = HashMap::new();
+    let mut rates = HashMap::new();
+    let mut visited = HashSet::new();
+    while let Some((valve, rate, adjacent)) = read_valve() {
+        graph.insert(valve.clone(), adjacent);
+        rates.insert(valve.clone(), rate);
+
+        if rate == 0 {
+            visited.insert(valve);
+        }
+    }
+
+    let num_of_valves = graph.len();
+    let mut start_valve = "AA".to_string();
+    let mut time = 30;
+
+    loop {
+        (start_valve, time) = bfs(&start_valve, time, &graph, &rates, &visited);
+        visited.insert(start_valve.clone());
+
+        if time == 0 || visited.len() == num_of_valves {
+            break;
+        }
+    }
 }
