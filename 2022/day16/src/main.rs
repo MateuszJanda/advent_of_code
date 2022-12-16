da798ae9e106248882664eff0de2d17c9c6de766
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-16 20:14:57 +0100

    Greedy approach

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -40,63 +40,67 @@ fn read_valve() -> Option<(String, i32, Vec<String>)> {
 
 fn bfs(
     start_valve: &String,
-    mut time: i32,
+    mut time_passed: i32,
     graph: &HashMap<String, Vec<String>>,
     rates: &HashMap<String, i32>,
-    visited: &HashSet<String>,
+    open: &HashSet<String>,
 ) -> (String, i32) {
+    let mut visited = HashSet::new();
     let mut queue = VecDeque::new();
+    queue.push_back((start_valve.clone(), time_passed));
 
-    time -= 1;
-    queue.push_back((start_valve.clone(), time));
-
-    let mut best_time = 0;
     let mut best_rate = 0;
     let mut best_valve = "".to_string();
 
     while let Some((valve, t)) = queue.pop_front() {
-        if visited.contains(&valve) || t - 1 == 0 {
-            continue;
+        if t >= TIME_LIMIT {
+            break;
         }
 
-        let rate = rates[&valve] * (t - 1);
-        if rate > best_rate {
+        let rate = rates[&valve] * (TIME_LIMIT - t);
+        if !open.contains(&valve) && rate > best_rate {
+            println!(" Rate {} {} {}", valve, rate, t);
             best_rate = rate;
-            best_time = t;
+            time_passed = t;
             best_valve = valve.clone();
         }
 
+        visited.insert(valve.clone());
+
         for v in graph[&valve].iter() {
-            queue.push_back((v.clone(), t - 1));
+            if !visited.contains(v) {
+                queue.push_back((v.clone(), t + 1));
+            }
         }
     }
 
-    (best_valve, best_time)
+    (best_valve, time_passed)
 }
+
+const TIME_LIMIT: i32 = 30;
 
 fn main() {
     let mut graph = HashMap::new();
     let mut rates = HashMap::new();
-    let mut visited = HashSet::new();
+    let mut open = HashSet::new();
     while let Some((valve, rate, adjacent)) = read_valve() {
         graph.insert(valve.clone(), adjacent);
         rates.insert(valve.clone(), rate);
 
         if rate == 0 {
-            visited.insert(valve);
+            open.insert(valve);
         }
     }
 
     let num_of_valves = graph.len();
     let mut start_valve = "AA".to_string();
-    let mut time = 30;
+    let mut time_passed = 1;
 
-    loop {
-        (start_valve, time) = bfs(&start_valve, time, &graph, &rates, &visited);
-        visited.insert(start_valve.clone());
+    // Greedy
+    while time_passed <= TIME_LIMIT && open.len() != num_of_valves {
+        (start_valve, time_passed) = bfs(&start_valve, time_passed, &graph, &rates, &open);
+        open.insert(start_valve.clone());
 
-        if time == 0 || visited.len() == num_of_valves {
-            break;
-        }
+        println!("Open {} {}", start_valve, time_passed);
     }
 }
