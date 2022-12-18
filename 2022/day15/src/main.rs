6df0945c449fcb5739175172aa41f3fbea11e05d
Mateusz Janda <mateusz.janda@gmail.com>
2022-12-18 15:41:12 +0100

    Calculate range - wrong answer

--- 2022/day15/src/main.rs
+++ 2022/day15/src/main.rs
@@ -1,7 +1,7 @@
 // Author:  mateusz.janda@gmail.com
 // Ad maiorem Dei gloriam
 
-use std::io;
+use std::{collections::BTreeSet, io};
 
 fn read_y() -> Option<i32> {
     let mut line = String::new();
@@ -57,19 +57,21 @@ fn read_data() -> Option<(Position, Position)> {
 }
 
 fn manhattan_distance(pos1: &Position, pos2: &Position) -> i32 {
-    let r = (pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs();
-    // println!("{:?} {:?} {}", pos1, pos2, r);
-    r
+    (pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs()
 }
 
 fn manhattan_horiz(pos1: &Position, pos2: &Position) -> i32 {
     (pos1.x - pos2.x).abs()
 }
 
-fn range_length(data: &Vec<(Position, Position)>, y_level: i32) -> i32 {
-    let mut x_min = None;
-    let mut x_max = None;
+#[derive(Ord, Eq, PartialEq, PartialOrd, Clone, Debug)]
+struct Segment {
+    x1: i32,
+    x2: i32,
+}
 
+fn build_segments(data: &Vec<(Position, Position)>, y_level: i32) -> BTreeSet<Segment> {
+    let mut segments = BTreeSet::new();
     for (sensor, beacon) in data {
         let distance = manhattan_distance(sensor, beacon);
 
@@ -77,48 +79,54 @@ fn range_length(data: &Vec<(Position, Position)>, y_level: i32) -> i32 {
             continue;
         }
 
-        println!("Pos {:?} {:?} {} {} {}", sensor, beacon, distance, sensor.x - distance, sensor.x + distance);
-
-        x_min = match x_min {
-            Some(x) => Some(std::cmp::min(x, sensor.x - manhattan_horiz(sensor, beacon))),
-            None => Some(sensor.x - distance),
+        let horiz_dist = manhattan_horiz(sensor, beacon);
+        let segment = Segment {
+            x1: sensor.x - horiz_dist,
+            x2: sensor.x + horiz_dist,
         };
 
-        x_max = match x_max {
-            Some(x) => Some(std::cmp::max(x, sensor.x + manhattan_horiz(sensor, beacon))),
-            None => Some(sensor.x + distance),
-        };
+        println!("Pos {:?} {:?} {:?}", sensor, beacon, segment);
+
+        segments.insert(segment);
     }
 
-    println!("{:?} {:?}", x_min, x_max);
+    segments
+}
 
-    x_max.unwrap() - x_min.unwrap()
+fn calc_range(segments: &BTreeSet<Segment>) -> i32 {
+    let mut x1 = None;
+    let mut x2 = None;
+    let mut result = 0;
+
+    for segment in segments {
+        println!("Segment {:?} {:?} {:?} ", segment, x1, x2);
+        (x1, x2) = match (x1, x2) {
+            (None, None) => (Some(segment.x1), Some(segment.x2)),
+            (Some(x_start), Some(x_end)) => match segment.x1 > x_end {
+                true => {
+                    result += x_end - x_start + 1;
+                    (Some(segment.x1), Some(segment.x2))
+                }
+                false => (Some(x_start), Some(std::cmp::max(x_end, segment.x2))),
+            },
+            _ => panic!("Unsupported case."),
+        }
+
+    }
+    result += x2.unwrap() - x1.unwrap() + 1;
+
+    result
 }
 
 fn main() {
     let y_level = read_y().unwrap();
-    // let mut x_min = None;
-    // let mut x_max = None;
     let mut data = vec![];
 
     while let Some((sensor, beacon)) = read_data() {
-        // x_min = match x_min {
-        //     Some(x) => Some(*vec![x, sensor.x, beacon.x].iter().min().unwrap()),
-        //     None => Some(std::cmp::min(sensor.x, beacon.x)),
-        // };
-
-        // x_max = match x_max {
-        //     Some(x) => Some(*vec![x, sensor.x, beacon.x].iter().max().unwrap()),
-        //     None => Some(std::cmp::max(sensor.x, beacon.x)),
-        // };
-
         data.push((sensor, beacon));
     }
 
-    // let range_length = x_max.unwrap() = x_min.unwrap();
-
-    // [-4, 26]
-    range_length(&data, y_level);
-
-    // println!("{:?} {:?}", x_min, x_max)
+    let segments = build_segments(&data, y_level);
+    let result1 = calc_range(&segments);
+    println!("{}", result1);
 }
